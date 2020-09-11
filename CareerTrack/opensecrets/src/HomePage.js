2b54464f7208231bff79de59f60623d72590012c
internetcharles <charlesodellsmith@gmail.com>
2020-09-11 15:01:18 -0700

    got net worth

--- CareerTrack/opensecrets/src/HomePage.js
+++ CareerTrack/opensecrets/src/HomePage.js
@@ -6,6 +6,7 @@ export default class HomePage extends Component {
     state = ({
         politicians: [],
         politicianContributors: [],
+        politicianFinancials: [],
         render: false
     })
 
@@ -22,12 +23,19 @@ export default class HomePage extends Component {
         this.setState({ politicians: parsedPoliticians.response.legislator });
 
         const politiciansWithContributions = [];
+        const politiciansFinancialInfo = [];
         this.state.politicians.forEach(async (politician, idx) => {
             const politicianContributions = await request.get(`https://www.opensecrets.org/api/?method=candContrib&cid=${politician["@attributes"].cid}&cycle=2020&output=json&apikey=e09f593c7520341a584d8bac1033a2d7`)
             let parsedPC =  JSON.parse(politicianContributions.text)
-            politiciansWithContributions[idx] = parsedPC.response.contributors;
+            politiciansWithContributions.push(parsedPC.response.contributors);
+
+            const politicianFinData = await request.get(`http://www.opensecrets.org/api/?method=memPFDprofile&year=2016&cid=${politician["@attributes"].cid}&output=json&apikey=e09f593c7520341a584d8bac1033a2d7`)
+            let parsedFD = JSON.parse(politicianFinData.text)
+            politiciansFinancialInfo.push(parsedFD.response.member_profile["@attributes"]);
         });
-        this.setState({ politicianContributors: politiciansWithContributions })
+        await this.setState({ politicianContributors: politiciansWithContributions,
+                        politicianFinancials: politiciansFinancialInfo })
+        console.log(this.state.politicianFinancials)
         console.log(this.state.politicianContributors)
         
     }
@@ -39,6 +47,7 @@ export default class HomePage extends Component {
                 this.state.politicianContributors.map(p => {
                     return <div>
                         <div>{p["@attributes"].cand_name}</div>
+                        <div>Net Worth: ${this.state.politicianFinancials.find(pol => pol.member_id === p["@attributes"].cid).net_high}</div>
                         <div>{p["contributor"].map((c, idx) => {
                         return <div>
                             <ul>{idx + 1}. {c["@attributes"].org_name}</ul>
